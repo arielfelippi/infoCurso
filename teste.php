40b9c13fe4b5d3ed2c04ee05d8d0d15cf5dea9de
ariel felippi <ariel.felippi@hotmail.com>
2021-10-05 21:36:24 -0300

    teste conflito 2

--- teste.php
+++ teste.php
@@ -1,8 +1,7 @@
 <?php
-
 echo "teste do merge";
-echo "teste do merge";
-echo "teste do merge";
+echo "teste do sdsdsdsd";
+echo "sdsdsds do merge";
 echo "teste do merge";
 echo "teste do merge";
 echo "Ola mundo";
